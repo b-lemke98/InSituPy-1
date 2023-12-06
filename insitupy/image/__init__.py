ee124170bd2ed81b247d4d477cba45cb48ed6703
jwrth <j.wirth@tum.de>
2023-12-06 16:48:50 +0100

    fix import error

--- insitupy/image/__init__.py
+++ insitupy/image/__init__.py
@@ -2,4 +2,4 @@ from .axes import ImageAxes
 from .io import write_ome_tiff
 from .registration import ImageRegistration
 from .utils import (convert_to_8bit, deconvolve_he, fit_image_to_size_limit,
-                    scale_to_max_width)
+                    scale_to_max_width, resize_image)
