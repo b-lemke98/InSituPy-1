610c1b5cf6bf05149df18a72d3a92e7fbb1bf349
jwrth <j.wirth@tum.de>
2023-12-06 11:33:42 +0100

    tested notebooks after restructuring

--- tests/test_parse_geopandas.py
+++ tests/test_parse_geopandas.py
@@ -7,12 +7,14 @@ d = {'id': ['i'],
     'objectType': ['o'], 
     'name': ['n'],
     'color': ['c'],
-    'geometry': [Point(0, 1)]
+    'geometry': [Point(0, 1)],
+    'origin': ['manual']
     }
 
 pdf = pd.DataFrame(d)
 
 result_df = GeoDataFrame(d, geometry=d["geometry"])
+result_df = result_df.set_index("id")
 
 def test_dict_geopandas():
     assert parse_geopandas(d).equals(result_df)
