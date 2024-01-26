f365a34a1ddb6c4ebc729205ba6085c90024d94d
jwrth <j.wirth@tum.de>
2024-01-26 14:37:14 +0100

    sumarize annotation addition in `_add_annotations_to_layer` function

--- insitupy/_core/_widgets.py
+++ insitupy/_core/_widgets.py
@@ -14,6 +14,7 @@ from scipy.sparse import issparse
 from shapely.geometry.multipolygon import MultiPolygon
 from shapely.geometry.polygon import LinearRing, Polygon
 
+from ._layers import _add_annotations_as_layer
 from ..utils.palettes import CustomPalettes
 from ..utils.utils import convert_to_list
 
@@ -309,70 +310,19 @@ def _initialize_point_widgets(
             
             # iterate through classes
             for cl in classes:
-                
+                # generate layer name
                 layer_name = f"*{cl} ({key})"
                 
                 if layer_name not in viewer.layers:
                     # get dataframe for this class
                     class_df = annot_df[annot_df["name"] == cl]
                     
-                    # iterate through annotations of this class and collect them as list
-                    shape_list = []
-                    color_list = []
-                    uid_list = []
-                    type_list = [] # list to store whether the polygon is exterior or interior
-                    for uid, row in class_df.iterrows():
-                        # get coordinates
-                        polygon = row["geometry"]
-                        #uid = row["id"]
-                        hexcolor = rgb2hex([elem / 255 for elem in row["color"]])
-                        
-                        # check if polygon is a MultiPolygon or just a simple Polygon object
-                        if isinstance(polygon, MultiPolygon):
-                            poly_list = list(polygon.geoms)
-                        elif isinstance(polygon, Polygon):
-                            poly_list = [polygon]
-                        else:
-                            raise ValueError(f"Input must be a Polygon or MultiPolygon object. Received: {type(polygon)}")
-                        
-                        for p in poly_list:
-                            # extract exterior coordinates from shapely object
-                            # Note: the last coordinate is removed since it is identical with the first
-                            # in shapely objects, leading sometimes to visualization bugs in napari
-                            exterior_array = np.array([p.exterior.coords.xy[1].tolist()[:-1],
-                                                    p.exterior.coords.xy[0].tolist()[:-1]]).T
-                            #exterior_array *= pixel_size # convert to length unit
-                            shape_list.append(exterior_array)  # collect shape
-                            color_list.append(hexcolor)  # collect corresponding color
-                            uid_list.append(uid)  # collect corresponding unique id
-                            type_list.append("exterior")
-                            
-                            # if polygon has interiors, plot them as well
-                            # for information on donut-shaped polygons in napari see: https://forum.image.sc/t/is-it-possible-to-generate-doughnut-shapes-in-napari-shapes-layer/88834
-                            if len(p.interiors) > 0:
-                                for linear_ring in p.interiors:
-                                    if isinstance(linear_ring, LinearRing):
-                                        interior_array = np.array([linear_ring.coords.xy[1].tolist()[:-1],
-                                                                linear_ring.coords.xy[0].tolist()[:-1]]).T
-                                        #interior_array *= pixel_size # convert to length unit
-                                        shape_list.append(interior_array)  # collect shape
-                                        color_list.append(hexcolor)  # collect corresponding color
-                                        uid_list.append(uid)  # collect corresponding unique id
-                                        type_list.append("interior")
-                                    else:
-                                        ValueError(f"Input must be a LinearRing object. Received: {type(linear_ring)}")
-                        
-                    viewer.add_shapes(shape_list, 
-                                    name=layer_name,
-                                    properties={
-                                        'uid': uid_list, # list with uids
-                                        'type': type_list # list giving information on whether the polygon is interior or exterior
-                                    },
-                                    shape_type='polygon', 
-                                    edge_width=10,
-                                    edge_color=color_list,
-                                    face_color='transparent'
-                                    )
+                    # add layer to viewer
+                    _add_annotations_as_layer(
+                        dataframe=class_df,
+                        viewer=viewer,
+                        layer_name=layer_name
+                    )
             
         # connect key change with update function
         @add_annotations_widget.key.changed.connect
--- insitupy/_core/xeniumdata.py
+++ insitupy/_core/xeniumdata.py
@@ -42,6 +42,7 @@ from ..utils.io import check_overwrite, read_json, write_dict_to_json
 from ..utils.utils import convert_to_list, decode_robust_series
 from ..utils.utils import textformat as tf
 from ._checks import check_raw
+from ._layers import _add_annotations_as_layer
 from ._scanorama import scanorama
 from ._widgets import (_annotation_widget, _create_points_layer,
                        _initialize_point_widgets)
@@ -1294,66 +1295,77 @@ class XeniumData:
                 
                 # iterate through classes
                 for cl in classes:
+                    # generate layer name
+                    layer_name = f"*{cl} ({annotation_key})"
+                    
                     # get dataframe for this class
                     class_df = annot_df[annot_df["name"] == cl]
                     
-                    # iterate through annotations of this class and collect them as list
-                    shape_list = []
-                    color_list = []
-                    uid_list = []
-                    type_list = [] # list to store whether the polygon is exterior or interior
-                    for uid, row in class_df.iterrows():
-                        # get coordinates
-                        polygon = row["geometry"]
-                        #uid = row["id"]
-                        hexcolor = rgb2hex([elem / 255 for elem in row["color"]])
+                    if layer_name not in self.viewer.layers:
+                        # add layer to viewer
+                        _add_annotations_as_layer(
+                            dataframe=class_df,
+                            viewer=self.viewer,
+                            layer_name=layer_name
+                        )
+                    
+                    # # iterate through annotations of this class and collect them as list
+                    # shape_list = []
+                    # color_list = []
+                    # uid_list = []
+                    # type_list = [] # list to store whether the polygon is exterior or interior
+                    # for uid, row in class_df.iterrows():
+                    #     # get coordinates
+                    #     polygon = row["geometry"]
+                    #     #uid = row["id"]
+                    #     hexcolor = rgb2hex([elem / 255 for elem in row["color"]])
                         
-                        # check if polygon is a MultiPolygon or just a simple Polygon object
-                        if isinstance(polygon, MultiPolygon):
-                            poly_list = list(polygon.geoms)
-                        elif isinstance(polygon, Polygon):
-                            poly_list = [polygon]
-                        else:
-                            raise ValueError(f"Input must be a Polygon or MultiPolygon object. Received: {type(polygon)}")
+                    #     # check if polygon is a MultiPolygon or just a simple Polygon object
+                    #     if isinstance(polygon, MultiPolygon):
+                    #         poly_list = list(polygon.geoms)
+                    #     elif isinstance(polygon, Polygon):
+                    #         poly_list = [polygon]
+                    #     else:
+                    #         raise ValueError(f"Input must be a Polygon or MultiPolygon object. Received: {type(polygon)}")
                         
-                        for p in poly_list:
-                            # extract exterior coordinates from shapely object
-                            # Note: the last coordinate is removed since it is identical with the first
-                            # in shapely objects, leading sometimes to visualization bugs in napari
-                            exterior_array = np.array([p.exterior.coords.xy[1].tolist()[:-1],
-                                                    p.exterior.coords.xy[0].tolist()[:-1]]).T
-                            #exterior_array *= pixel_size # convert to length unit
-                            shape_list.append(exterior_array)  # collect shape
-                            color_list.append(hexcolor)  # collect corresponding color
-                            uid_list.append(uid)  # collect corresponding unique id
-                            type_list.append("exterior")
+                    #     for p in poly_list:
+                    #         # extract exterior coordinates from shapely object
+                    #         # Note: the last coordinate is removed since it is identical with the first
+                    #         # in shapely objects, leading sometimes to visualization bugs in napari
+                    #         exterior_array = np.array([p.exterior.coords.xy[1].tolist()[:-1],
+                    #                                 p.exterior.coords.xy[0].tolist()[:-1]]).T
+                    #         #exterior_array *= pixel_size # convert to length unit
+                    #         shape_list.append(exterior_array)  # collect shape
+                    #         color_list.append(hexcolor)  # collect corresponding color
+                    #         uid_list.append(uid)  # collect corresponding unique id
+                    #         type_list.append("exterior")
                             
-                            # if polygon has interiors, plot them as well
-                            # for information on donut-shaped polygons in napari see: https://forum.image.sc/t/is-it-possible-to-generate-doughnut-shapes-in-napari-shapes-layer/88834
-                            if len(p.interiors) > 0:
-                                for linear_ring in p.interiors:
-                                    if isinstance(linear_ring, LinearRing):
-                                        interior_array = np.array([linear_ring.coords.xy[1].tolist()[:-1],
-                                                                linear_ring.coords.xy[0].tolist()[:-1]]).T
-                                        #interior_array *= pixel_size # convert to length unit
-                                        shape_list.append(interior_array)  # collect shape
-                                        color_list.append(hexcolor)  # collect corresponding color
-                                        uid_list.append(uid)  # collect corresponding unique id
-                                        type_list.append("interior")
-                                    else:
-                                        ValueError(f"Input must be a LinearRing object. Received: {type(linear_ring)}")
+                    #         # if polygon has interiors, plot them as well
+                    #         # for information on donut-shaped polygons in napari see: https://forum.image.sc/t/is-it-possible-to-generate-doughnut-shapes-in-napari-shapes-layer/88834
+                    #         if len(p.interiors) > 0:
+                    #             for linear_ring in p.interiors:
+                    #                 if isinstance(linear_ring, LinearRing):
+                    #                     interior_array = np.array([linear_ring.coords.xy[1].tolist()[:-1],
+                    #                                             linear_ring.coords.xy[0].tolist()[:-1]]).T
+                    #                     #interior_array *= pixel_size # convert to length unit
+                    #                     shape_list.append(interior_array)  # collect shape
+                    #                     color_list.append(hexcolor)  # collect corresponding color
+                    #                     uid_list.append(uid)  # collect corresponding unique id
+                    #                     type_list.append("interior")
+                    #                 else:
+                    #                     ValueError(f"Input must be a LinearRing object. Received: {type(linear_ring)}")
                         
-                    self.viewer.add_shapes(shape_list, 
-                                    name=f"*{cl} ({annotation_key})",
-                                    properties={
-                                        'uid': uid_list, # list with uids
-                                        'type': type_list # list giving information on whether the polygon is interior or exterior
-                                    },
-                                    shape_type='polygon', 
-                                    edge_width=10,
-                                    edge_color=color_list,
-                                    face_color='transparent'
-                                    )
+                    # self.viewer.add_shapes(shape_list, 
+                    #                 name=f"*{cl} ({annotation_key})",
+                    #                 properties={
+                    #                     'uid': uid_list, # list with uids
+                    #                     'type': type_list # list giving information on whether the polygon is interior or exterior
+                    #                 },
+                    #                 shape_type='polygon', 
+                    #                 edge_width=10,
+                    #                 edge_color=color_list,
+                    #                 face_color='transparent'
+                    #                 )
                     
         # WIDGETS
         try:
