5f9c7ac2f63c3e36cf63d59c5a4122218b43e3eb
aitanae <aitanaegea2099@gmail.com>
2024-08-19 14:16:57 +0200

    Added layer argument to reduce_dimensions_anndata() function

--- insitupy/utils/preprocessing.py
+++ insitupy/utils/preprocessing.py
@@ -37,9 +37,11 @@ def normalize_anndata(adata,
     else:
         raise ValueError(f'`transformation_method` is not one of ["log1p", "sqrt"]')
 
+
 def reduce_dimensions_anndata(adata,
                               umap: bool = True,
                               tsne: bool = False,
+                              layer: Optional[str] = None,
                               batch_correction_key: Optional[str] = None,
                               perform_clustering: bool = True,
                               verbose: bool = True,
@@ -55,6 +57,8 @@ def reduce_dimensions_anndata(adata,
             If True, perform UMAP dimensionality reduction. Default is True.
         tsne (bool, optional):
             If True, perform t-SNE dimensionality reduction. Default is True.
+        layer (str, optional): 
+            Specifies the layer of the AnnData object to operate on. Default is None (uses adata.X).
         batch_correction_key (str, optional):
             Batch key for performing batch correction using scanorama. Default is None, indicating no batch correction.
         verbose (bool, optional):
@@ -73,38 +77,68 @@ def reduce_dimensions_anndata(adata,
         None: This method modifies the input matrix in place, reducing its dimensionality using specified techniques and
             batch correction if applicable. It does not return any value.
     """
+    
+    # Determine the prefix for the data
+    data_prefix = layer if layer else "X"
+
     if batch_correction_key is None:
         # dimensionality reduction
         print("Dimensionality reduction...") if verbose else None
-        sc.pp.pca(adata)
+
+        # perform PCA with the specified layer
+        sc.pp.pca(adata, layer=layer)
+
+        # Manually rename the PCA results with the prefix. Future scanpy version will include an argument
+        # key_added to do this automatically
+        adata.obsm[f'{data_prefix}_pca'] = adata.obsm['X_pca']
+        del adata.obsm['X_pca']
+
+        adata.varm[f'{data_prefix}_PCs'] = adata.varm['PCs']
+        del adata.varm['PCs']
+
+        adata.uns[f'{data_prefix}_pca'] = adata.uns['pca']
+        del adata.uns['pca']
+        
         if umap:
-            sc.pp.neighbors(adata)
-            sc.tl.umap(adata)
+            # Perform neighbors analysis with the specified prefix
+            sc.pp.neighbors(adata, use_rep=f'{data_prefix}_pca', key_added=f'{data_prefix}_neighbors')
+
+            # Perform UMAP using the custom neighbors key
+            sc.tl.umap(adata, neighbors_key=f'{data_prefix}_neighbors')
+            
+            # Rename and store UMAP results with the appropriate prefix
+            adata.obsm[f'{data_prefix}_umap'] = adata.obsm['X_umap']
+            del adata.obsm['X_umap']
+
+            adata.uns[f'{data_prefix}_umap'] = adata.uns['umap']
+            del adata.uns['umap']
+        
         if tsne:
-            sc.tl.tsne(adata, n_jobs=tsne_jobs, learning_rate=tsne_lr)
+            # Perform t-SNE using the PCA results with the specified prefix
+            sc.tl.tsne(adata, n_jobs=tsne_jobs, learning_rate=tsne_lr, use_rep=f'{data_prefix}_pca', key_added=f'{data_prefix}_tsne')
 
     else:
-        # PCA
-        sc.pp.pca(adata)
+        # PCA for batch correction
+        sc.pp.pca(adata, layer=layer)
 
-        neigh_uncorr_key = 'neighbors_uncorrected'
-        sc.pp.neighbors(adata, key_added=neigh_uncorr_key)
+        neigh_uncorr_key = f'{data_prefix}_neighbors_uncorrected'
+        sc.pp.neighbors(adata, use_rep=f'{data_prefix}_pca', key_added=neigh_uncorr_key)
 
         if perform_clustering:
-            # clustering
-            sc.tl.leiden(adata, neighbors_key=neigh_uncorr_key, key_added='leiden_uncorrected')
+            # Clustering
+            sc.tl.leiden(adata, neighbors_key=neigh_uncorr_key, key_added=f'{data_prefix}_leiden_uncorrected')
 
-        # batch correction
+        # Batch correction
         print(f"Batch correction using scanorama for {batch_correction_key}...") if verbose else None
         hvgs = list(adata.var_names[adata.var['highly_variable']])
         adata = scanorama(adata, batch_key=batch_correction_key, hvg=hvgs, verbose=False, **kwargs)
 
-        # find neighbors
-        sc.pp.neighbors(adata, use_rep="X_scanorama")
-        sc.tl.umap(adata)
-        sc.tl.tsne(adata, use_rep="X_scanorama")
+        # Find neighbors and reduce dimensions
+        sc.pp.neighbors(adata, use_rep="X_scanorama", key_added=f'{data_prefix}_scanorama_neighbors')
+        sc.tl.umap(adata, neighbors_key=f'{data_prefix}_scanorama_neighbors', key_added=f'{data_prefix}_scanorama_umap')
+        sc.tl.tsne(adata, use_rep="X_scanorama", key_added=f'{data_prefix}_scanorama_tsne')
 
     if perform_clustering:
-        # clustering
+        # Clustering
         print("Leiden clustering...") if verbose else None
-        sc.tl.leiden(adata)
+        sc.tl.leiden(adata, neighbors_key=f'{data_prefix}_neighbors', key_added=f'{data_prefix}_leiden')
