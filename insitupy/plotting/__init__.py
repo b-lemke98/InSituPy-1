6de357294820f9603c7c0e53907f881ee413334e
Johannes Wirth <j.wirth@tum.de>
2024-12-11 18:08:56 +0100

    fixes in plotting functionalities

--- insitupy/plotting/__init__.py
+++ insitupy/plotting/__init__.py
@@ -1,4 +1,5 @@
 from .expression_along_axis import (cell_abundance_along_axis,
                                     cell_expression_along_axis,
                                     expr_along_obs_val)
+from .plots import plot_cell_composition, plot_colorlegend
 from .volcano import volcano_plot
