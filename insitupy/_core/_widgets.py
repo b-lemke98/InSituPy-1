291fc0dabb738315cd4405f603024056b8767e35
Johannes Wirth <j.wirth@tum.de>
2025-03-26 10:49:07 +0100

    small fix

--- insitupy/_core/_widgets.py
+++ insitupy/_core/_widgets.py
@@ -42,7 +42,8 @@ if WITH_NAPARI:
             filter_cells_widget = None
         else:
             # initialize data_name of viewer
-            config.init_data_name(xdata=xdata)
+            _config.init_data_name(xdata=xdata)
+
             # initialize viewer configuration
             _config.init_viewer_config(xdata=xdata,
                                         #data_name=config.current_data_name
