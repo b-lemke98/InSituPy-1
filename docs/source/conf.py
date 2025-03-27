59ef4517f6ae599b5dad725a626f83419948845a
Johannes Wirth <j.wirth@tum.de>
2025-03-27 07:05:57 +0100

    remove some myst extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,19 +58,11 @@ myst_heading_anchors = 3  # create anchors for h1-h3
 
 myst_enable_extensions = [
     "amsmath",
-    "attrs_inline",
     "colon_fence",
     "deflist",
     "dollarmath",
-    "fieldlist",
-    "html_admonition",
     "html_image",
-    "linkify",
-    "replacements",
-    "smartquotes",
-    "strikethrough",
-    "substitution",
-    "tasklist",
+    "html_admonition",
 ]
 
 myst_url_schemes = ("http", "https", "mailto")
