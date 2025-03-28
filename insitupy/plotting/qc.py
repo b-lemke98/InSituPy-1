ba7c97857b6d64addc2a0e24d131997caf62a7b1
Johannes Wirth <j.wirth@tum.de>
2025-03-28 11:41:56 +0100

    allow AnnData in plot_qc_metrics

--- insitupy/plotting/qc.py
+++ insitupy/plotting/qc.py
@@ -15,68 +15,9 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from insitupy._core._checks import check_integer_counts
 from insitupy._core._utils import _get_cell_layer
 
-# def plot_qc_metrics(
-#     data: InSituData, # type: ignore #TODO: Expand this to InSituExperiment
-#     cells_layer: Optional[str] = None,
-#     ):
-#     """
-#     Plots the QC metrics calculated by sc.pp.calculate_qc_metrics.
-
-#     Parameters:
-#     adata : AnnData
-#         Annotated data matrix with QC metrics calculated.
-#     """
-#     # retrieve AnnData from cell layer
-#     celldata = _get_cell_layer(cells=data.cells, cells_layer=cells_layer)
-#     adata = celldata.matrix
-
-#     # QC metrics in .obs
-#     obs_metrics = ['total_counts', 'n_genes_by_counts', 'pct_counts_mt']
-#     # QC metrics in .var
-#     var_metrics = ['n_cells_by_counts', 'mean_counts', 'pct_dropout_by_counts', 'total_counts']
-
-#     # Check if all metrics exist in .obs
-#     obs_metrics = [metric for metric in obs_metrics if metric in adata.obs]
-#     if len(obs_metrics) == 0:
-#         print("Warning: No .obs metrics found in adata.obs")
-
-#     # Check if all metrics exist in .var
-#     var_metrics = [metric for metric in var_metrics if metric in adata.var]
-#     if len(var_metrics) == 0:
-#         print("Warning: No .var metrics found in adata.var")
-
-#     ncols = len(obs_metrics+var_metrics)
-#     nrows = 1
-#     fig, axes = plt.subplots(nrows, ncols, figsize=(5*ncols, 5*nrows))
-
-#     # Add titles to each row
-#     if len(obs_metrics) > 0:
-#         axes[0].annotate('.obs Metrics', xy=(0, 0.5), xytext=(-axes[0].yaxis.labelpad - 5, 0),
-#                             xycoords=axes[0].yaxis.label, textcoords='offset points',
-#                             size='large', ha='right', va='center', rotation=90, weight='bold')
-
-#     if len(var_metrics) > 0:
-#         axes[2].annotate('.var Metrics', xy=(0, 0.5), xytext=(-axes[2].yaxis.labelpad - 5, 0),
-#                             xycoords=axes[2].yaxis.label, textcoords='offset points',
-#                             size='large', ha='right', va='center', rotation=90, weight='bold')
-
-#     for i, metric in enumerate(obs_metrics):
-#         sns.histplot(adata.obs[metric], bins=50, color='skyblue', edgecolor='black', kde=False, ax=axes[i])
-#         axes[i].set_title(metric)
-#         axes[i].set_xlabel('Value')
-#         axes[i].set_ylabel('Frequency')
-
-#     for i, metric in enumerate(var_metrics):
-#         sns.histplot(adata.var[metric], bins=50, color='coral', edgecolor='black', kde=False, ax=axes[2+i])
-#         axes[2+i].set_title(metric)
-#         axes[2+i].set_xlabel('Value')
-#         axes[2+i].set_ylabel('Frequency')
-
-#     plt.tight_layout(rect=[0, 0, 1, 1])
-#     plt.show()
 
 def plot_qc_metrics(
-    data: InSituData, # type: ignore #TODO: Expand this to InSituExperiment
+    data: Union[InSituData, AnnData], # type: ignore #TODO: Expand this to InSituExperiment
     cells_layer: Optional[str] = None,
     show_inset: bool = True,
     inset_fraction: Number = 0.2
@@ -88,9 +29,12 @@ def plot_qc_metrics(
     adata : AnnData
         Annotated data matrix with QC metrics calculated.
     """
-    # retrieve AnnData from cell layer
-    celldata = _get_cell_layer(cells=data.cells, cells_layer=cells_layer)
-    adata = celldata.matrix
+    if isinstance(data, AnnData):
+        adata = data.copy()
+    else:
+        # retrieve AnnData from cell layer
+        celldata = _get_cell_layer(cells=data.cells, cells_layer=cells_layer)
+        adata = celldata.matrix.copy()
 
     # QC metrics in .obs
     obs_metrics = ['total_counts', 'n_genes_by_counts', 'pct_counts_mt']
